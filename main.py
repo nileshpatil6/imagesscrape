c5127946c35aa266d68cb06c3f6893464b88596a
Nilesh Patil <128893479+nileshpatil6@users.noreply.github.com>
2025-05-25 21:58:45 +0530

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -184,4 +184,4 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app_updated:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
